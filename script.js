2d6c858a7b5f3410adbabc3d044ae33b6e635ea1
G.jahnavi <jahnavi2002g@gmail.com>
2024-05-18 01:12:43 +0530

    add

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('navigateButton').addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/sample3.html'; // Replace with the URL you want to navigate to
+});
+document.getElementById('navigateButton1').addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/sample3.html'; // Replace with the URL you want to navigate to
+});
+document.getElementById('navigateButton2').addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/sample3.html'; // Replace with the URL you want to navigate to
+});
+document.getElementById('navigateButton3').addEventListener('click', function() {
+    window.location.href = 'http://127.0.0.1:5500/sample3.html'; // Replace with the URL you want to navigate to
+});
